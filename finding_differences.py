e3fb4afae1430cb0747f9a818d209ded98a71d24
duyanh0208 <21020263@vnu.edu.vn>
2023-03-29 02:56:36 +0700

    version 2

--- finding_differences.py
+++ finding_differences.py
@@ -3,44 +3,43 @@ import imutils
 import numpy as np
 
 
-def spotting():
-    imagine1 = cv2.imread("output/Left.jpg")
-    imagine2 = cv2.imread("output/Right.jpg")
-    # blur_image = cv2.blur(imagine1, (5, 5))
-    # cv2.imshow('Blurred Image', blur_image)
-    imagine1 = imutils.resize(imagine1, height=460)
-    imagine2 = imutils.resize(imagine2, height=460)
-
-    img_height = imagine1.shape[0]
-
-    dissimilar = cv2.absdiff(imagine1, imagine2)
-    cv2.imshow("Dissimilar", dissimilar)
-
+def Thresh_Dilated(dissimilar):
     dissimilar = cv2.cvtColor(dissimilar, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow("Grey dissimilar", dissimilar)
     for i in range(0, 3):
         dilated = cv2.dilate(dissimilar.copy(), None, iterations=i + 1)
     (T, thresh) = cv2.threshold(dilated, 3, 255, cv2.THRESH_BINARY)
 
     kernel = np.ones((3, 3), np.uint8)
     dilate = cv2.dilate(thresh, kernel, iterations=5)
-    cv2.imshow("Dilate", dilate)
+    return dilate
+
+
+def spotting(level):
+    imagine1 = cv2.imread("output/left_"+ str(level) + ".jpg")
+    imagine2 = cv2.imread("output/right_"+ str(level) + ".jpg")
+    imagine1 = imutils.resize(imagine1, height=460)
+    imagine2 = imutils.resize(imagine2, height=460)
+
+    img_height = imagine1.shape[0]
+
+    dissimilar = cv2.absdiff(imagine1, imagine2)
+
+    dilate = Thresh_Dilated(dissimilar)
 
     edges = cv2.findContours(dilate.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     edges = imutils.grab_contours(edges)
     cnt = 0
     for edge in edges:
         if cv2.contourArea(edge) >= 60:
-            cnt = cnt + 1
+            cnt += 1
             x, y, w, h = cv2.boundingRect(edge)
             cv2.rectangle(imagine1, (x, y), (x + w, y + h), (0, 0, 255), 2)
             cv2.rectangle(imagine2, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     x = np.zeros((img_height, 10, 3), np.uint8)
     outcome = np.hstack((imagine1, x, imagine2))
-    print("Quantity of differences:", str(cnt))
-    cv2.imshow("Differences", outcome)
+    cv2.imshow("Showing differences", outcome)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-import argparse
-
 import finding_differences
 import makingdata
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='The script will spot the differences between two images, circle and number them. ')
-    parser.add_argument('--img', action='store', type=str, help='the path to image', default='imagines/titan1.jpg')
-    parser.add_argument('--level', action='store', type=int, help='level game', default=3)
-    parser.add_argument('--limit', action='store', type=int, help='limit difference', default=5)
-    # python main.py --img '' --level 2 --limit 10
-
-    # IMG1 = input('Enter path to image 1: ')
-    # IMG2 = input('Enter path to image 2: ')
-    args = parser.parse_args()
-    IMG = args.img
-    Level = args.level
-    Limit = args.limit
-    makingdata.processing_data()
-    finding_differences.spotting()
-    print("BuiDaoDuyAnh_21020263")
+    level = 3
+    path = "imagines/villain.jpg"
+    limit = 4
+    for i in range(level):
+        makingdata.processing_data(path, i + 1, limit)
+        finding_differences.spotting(i + 1)
